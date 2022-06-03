8762c79ab95d4606938e035ee999522cc1fac799
Sławomir Dziurman <g1.slawomir.dziurman@gmail.com>
2022-06-03 23:03:50 +0200

    getActual, getActualComments methods added

--- records/whatsTheMelody.record.ts
+++ records/whatsTheMelody.record.ts
@@ -1,3 +1,4 @@
+import { WhatsTheMelody } from "../models/whatsTheMelody";
 import { Comment, WhatsTheMelodyAPI, WTMVotes } from "../types";
 
 export class WhatsTheMelodyRecord implements WhatsTheMelodyAPI {
@@ -13,4 +14,16 @@ export class WhatsTheMelodyRecord implements WhatsTheMelodyAPI {
     ) {
 
     }
+
+    static async getActual(): Promise<WhatsTheMelodyAPI | null> {
+        const wtmLength = await WhatsTheMelody.countDocuments();
+        const actual = await WhatsTheMelody.find().skip(wtmLength > 0 ? wtmLength - 1 : wtmLength);
+        return actual.length > 0 ? actual[0] : null;
+    };
+
+    static async getActualComments(): Promise<Comment[] | null> {
+        const wtmLength = await WhatsTheMelody.countDocuments();
+        const actual: WhatsTheMelodyAPI[] = await WhatsTheMelody.find().skip(wtmLength > 0 ? wtmLength - 1 : wtmLength);
+        return actual.length > 0 ? actual[0].comments : null;
+    };
 }
