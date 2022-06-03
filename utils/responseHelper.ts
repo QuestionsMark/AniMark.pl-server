b7fe3ac2a0cc127f66063456088a63c6085e9570
Sławomir Dziurman <g1.slawomir.dziurman@gmail.com>
2022-06-03 23:05:44 +0200

    responseApiHelper added

--- utils/responseHelper.ts
+++ utils/responseHelper.ts
@@ -1,6 +1,11 @@
-import { AnyData, ServerResponse } from "../types";
+import { AnyData, ServerApiResponse, ServerResponse } from "../types";
 
 export const responseHelper = (message: string, results: AnyData | null = null): ServerResponse => {
     if (!results) return { message };
     return { message, results };
+};
+
+export const responseApiHelper = (results: AnyData | null, amount: number | null = null): ServerApiResponse => {
+    if (!amount) return { results };
+    return { results, amount };
 };
