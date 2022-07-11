65ca50d6fa8a32b726323f4a8e28e37006d7a6af
QuestionsMark <81152066+QuestionsMark@users.noreply.github.com>
2022-07-11 10:59:40 +0200

    Update config.example.ts

--- config/config.example.ts
+++ config/config.example.ts
@@ -1,6 +1,6 @@
 export const DB_CONNECTION = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]";     // link połączenia do bazy danych MongoDB
 
-export const TOKEN_SECRET = "KOf@h73KEU&4JjKJk2!Lo";     // dowolny ciąg znaków
+export const TOKEN_SECRET = "KOf@h73KEU&4JjKJk2!Lo";     // dowolny ciąg znaków, mozna zostawić tak jak jest
 
 export const HOST_ADDRESS = "http://localhost:3001";     // adres servera bez slasha na końcu!!!
 
