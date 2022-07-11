8f6d58683b88c3537cc3530230510bf80f7d78f7
QuestionsMark <81152066+QuestionsMark@users.noreply.github.com>
2022-07-11 12:11:13 +0200

    Update config.example.ts

--- config/config.example.ts
+++ config/config.example.ts
@@ -1,5 +1,5 @@
-export const DB_CONNECTION = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]";     // link połączenia do bazy danych MongoDB
-// export const DB_CONNECTION = "mongodb+srv://admin:tP7Nh6IJKw49d2Cg@cluster0.bsn6d.mongodb.net/test-db?retryWrites=true&w=majority";     // Baza danych z przykładowymi danymi
+// export const DB_CONNECTION = "mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]";     // link połączenia do bazy danych MongoDB
+export const DB_CONNECTION = "mongodb://localhost/test";
 
 export const TOKEN_SECRET = "KOf@h73KEU&4JjKJk2!Lo";     // dowolny ciąg znaków, mozna zostawić tak jak jest
 
